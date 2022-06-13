254d04cb39b1e832bb591b36f4fbd1e3bdcf0a83
gary camboulives <gary.camboulives@gmail.com>
2022-06-13 14:18:02 +0200

    overfetching solved with new action creator

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser} from '../actions';
+
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     const user = this.props.users.find((user) => user.id === this.props.userId);
 
@@ -22,4 +18,4 @@ const mapStateToProps = (state) => {
   return { users: state.users };
 };
 
-export default connect(mapStateToProps, {fetchUser})(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
